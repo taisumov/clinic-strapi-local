5e9bb70880b9ffccd5772ad61d4ad0fb496237a3
taisumov <islam.taisumov10@gmail.com>
2023-02-23 20:50:42 +0300

    commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '185.26.120.246'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
+  url: env('PUBLIC_URL', 'http://185.26.120.246:1337')
 });
